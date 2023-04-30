3f6fa6e09c0d50a56cdb80988357880696e8d750
yuldan14 <72258620+yuldan14@users.noreply.github.com>
2023-04-30 19:59:25 +0700

    mengedit dropdown menu

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const navbarNav=document.querySelector('.navbar-nav');
-document.querySelector ('#menu'). onclick= () =>  {
-    navbarNav.classList.toggle('active');
-};
-
 
 //click di luar sidebar
+
+
+const navbarMenu=document.querySelector('.navbar-menu');
+document.querySelector('#menu').onclick=()=>{
+    navbarMenu.classList.toggle('aktif')
+}
 const menu=document.querySelector('#menu');
 document.addEventListener('click', function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
+    if(!menu.contains(e.target) && !navbarMenu.contains(e.target)){
+        navbarMenu.classList.remove('aktif');
     }
 });
